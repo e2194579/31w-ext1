cc56dccf5cb7384920c7050a105fade808e146c7
Rbc <e2194579@cmaisonneuve.qc.ca>
2022-06-06 17:15:21 -0400

    s4-c6 Index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?> 
+<h1>Mon premier modèle Wordpress</h1>
+<?php
+if (have_posts()):
+    while(have_posts()) : the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content(); ?></p>
+<?php $lien = "<a href='" . get_permalink() . "'>Lien</a>";
+echo $lien;?>
+        <!-- <p><?= wp_trim_words(get_the_content(), 20, " ... ") ?> -->
+    <?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>

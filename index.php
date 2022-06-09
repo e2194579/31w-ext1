948411e8ebeb27d38994797c6649e1e671439241
Rbc <e2194579@cmaisonneuve.qc.ca>
2022-06-09 09:43:22 -0400

    s5-c7 index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?> 
-<h1>Mon premier modèle Wordpress</h1>
+<h1>index.php</h1>
 <?php
 if (have_posts()):
     while(have_posts()) : the_post(); ?>
         <h2><?php the_title(); ?></h2>
-        <p><?php the_content(); ?></p>
-<?php $lien = "<a href='" . get_permalink() . "'>Lien</a>";
+        <p><?= get_the_content()?></p>
+<?php $lien = "<a class='button' href='" . get_permalink() . "'>". substr(get_the_title(),0,8). "</a>";
 echo $lien;?>
-        <!-- <p><?= wp_trim_words(get_the_content(), 20, " ... ") ?> -->
+
     <?php endwhile; ?>
 <?php endif; ?>
 
